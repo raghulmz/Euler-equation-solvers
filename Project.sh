1bd5f04bdf2c74e8bed7c075f210793fae3537e3
raghulmz <raghulmz@gmail.com>
2015-02-21 20:49:30 +0530

    Update Project.sh

--- Project.sh
+++ Project.sh
@@ -1,3 +1,5 @@
-g++ -O3 Geometry.cpp Functions.cpp Boundary_Condition.cpp File_Dealing.cpp Tools.cpp Main.cpp -o Project #&& chmod +x Project
+mkdir ./Data
+
+g++ -O3 Geometry.cpp Functions.cpp Boundary_Condition.cpp File_Dealing.cpp Tools.cpp Main.cpp -o Project && chmod +x Project
 
 ./Project
