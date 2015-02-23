49a7f97be4a78b6217a042017c1c612586b73fc7
raghulmz <raghulmz@gmail.com>
2015-02-24 01:00:03 +0530

    Update double_grid.sh

--- double_grid.sh
+++ double_grid.sh
@@ -1,3 +1,7 @@
+!#/bin/bash
+
+mkdir ./Data
+
 g++ -O3 Geometry.cpp Functions.cpp Boundary_Condition.cpp File_Dealing.cpp Tools.cpp double_grid.cpp -o double_grid #&& chmod +x Project
 
 ./double_grid
